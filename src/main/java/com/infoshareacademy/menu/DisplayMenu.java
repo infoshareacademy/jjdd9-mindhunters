f74691288443bf7bab49843fdca32e7206e6550e
kamil-markowski <kamilszmarkowski@gmail.com>
2020-04-19 20:11:31 +0200

    Add frames in DisplayMenu

--- src/main/java/com/infoshareacademy/menu/DisplayMenu.java
+++ src/main/java/com/infoshareacademy/menu/DisplayMenu.java
@@ -6,33 +6,57 @@ public class DisplayMenu {
 
     public static void displayMainMenu() {
         clearConsole();
-        System.out.println("--------------- Main Menu ---------------");
-        System.out.println("ENTER [1] to browse drinks recipes \nENTER [2] to manage recipes \nENTER [3] to change " +
-                "settings \nENTER [4] to EXIT ");
+        System.out.println(" ------------------ Main Menu ----------------------");
+        System.out.println("|  ENTER [1] to browse drinks recipes               |" +
+                         "\n|  ENTER [2] to manage recipes                      |" +
+                         "\n|  ENTER [3] to change settings                     |" +
+                         "\n|  ENTER [4] to EXIT                                |");
+        System.out.println(" ---------------------------------------------------");
     }
 
     public static void displayBrowseMenu() {
         clearConsole();
-        System.out.println("--------------- Drink Browse Menu ---------------");
-        System.out.println("ENTER [1] to display all drinks list \nENTER [2] to search drink by name \nENTER [3] to search drink by ingredient \nENTER [4] to search drink by category \nENTER [5] to return to previous menu \nENTER [6] to EXIT ");
+        System.out.println(" --------------- Drink Browse Menu -----------------");
+        System.out.println("|  ENTER [1] to display all drinks list             |" +
+                         "\n|  ENTER [2] to search drink by name                |" +
+                         "\n|  ENTER [3] to search drink by ingredient          |" +
+                         "\n|  ENTER [4] to search drink by category            |" +
+                         "\n|  ENTER [5] to return to previous menu             |" +
+                         "\n|  ENTER [6] to EXIT                                |");
+        System.out.println(" ---------------------------------------------------");
+
     }
 
     public static void displayManageMenu() {
         clearConsole();
-        System.out.println("--------------- Drink Management Menu ---------------");
-        System.out.println("ENTER [1] to add drink \nENTER [2] to delete drink \nENTER [3] to add drink to favourites \nENTER [4] to remove drink from favourites \nENTER [5] to return to previous menu \nENTER [6] to EXIT ");
+        System.out.println(" ------------- Drink Management Menu ---------------");
+        System.out.println("|  ENTER [1] to add drink                           |" +
+                         "\n|  ENTER [2] to delete drink                        |" +
+                         "\n|  ENTER [3] to add drink to favourites             |" +
+                         "\n|  ENTER [4] to remove drink from favourites        |" +
+                         "\n|  ENTER [5] to return to previous menu             |" +
+                         "\n|  ENTER [6] to EXIT                                |");
+        System.out.println(" ---------------------------------------------------");
     }
 
     public static void displaySettingsMenu() {
         clearConsole();
-        System.out.println("--------------- Settings ---------------");
-        System.out.println("ENTER [1] to load/change configuration \nENTER [2] to change drinks sorting order \nENTER [3] to set recipe data modification format \nENTER [4] to return to previous menu \nENTER [5] to EXIT ");
+        System.out.println(" ------------------- Settings ----------------------");
+        System.out.println("|  ENTER [1] to load/change configuration           |" +
+                         "\n|  ENTER [2] to change drinks sorting order         |" +
+                         "\n|  ENTER [3] to set recipe data modification format |" +
+                         "\n|  ENTER [4] to return to previous menu             |" +
+                         "\n|  ENTER [5] to EXIT                                |");
+        System.out.println(" ---------------------------------------------------");
     }
 
     public static void displayReturnMenu() {
         clearConsole();
-        System.out.println("--------------- RETURN MENU ---------------");
-        System.out.println("ENTER [1] to return to previous menu \nENTER [2] to EXIT \nENTER [3] to stay in this menu");
+        System.out.println(" ----------------- RETURN MENU ---------------------");
+        System.out.println("|  ENTER [1] to return to previous menu             |" +
+                         "\n|  ENTER [2] to EXIT                                |" +
+                         "\n|  ENTER [3] to stay in this menu                   |");
+        System.out.println(" ---------------------------------------------------");
     }
 
     public static void clearConsole() {
