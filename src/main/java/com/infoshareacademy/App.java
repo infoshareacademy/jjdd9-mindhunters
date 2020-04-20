ce3258d25f59f943db379b44134d927c56522524
Lukasz Spyra <lukasz.spyrka@gmail.com>
2020-04-20 20:23:35 +0200

    Add number validation by ApacheCommonsLang3

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -7,6 +7,5 @@ public class App {
     public static void main(String[] args) {
         MenuControl menuControl = new MenuControl();
         menuControl.mainNavigation();
-
     }
 }
--- src/main/java/com/infoshareacademy/menu/UserInput.java
+++ src/main/java/com/infoshareacademy/menu/UserInput.java
@@ -1,5 +1,6 @@
 package com.infoshareacademy.menu;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,14 +10,18 @@ public class UserInput {
 
     private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
     private static final String CHOICE_MESSAGE = "\nType your choice: ";
+    private static final String WRONG_INPUT_MESSAGE = "\nWrong input. Try again. ";
 
     Scanner scanner = new Scanner(System.in);
 
     public int getUserInput() {
         STDOUT.info(CHOICE_MESSAGE);
         String userChoice = scanner.nextLine();
-
-        return Integer.parseInt(userChoice);
+        if (NumberUtils.isCreatable(userChoice)) {
+            return Integer.parseInt(userChoice);
+        } else {
+            STDOUT.info(WRONG_INPUT_MESSAGE);
+        }
+        return 0;
     }
-
 }
