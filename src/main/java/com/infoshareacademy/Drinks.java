154e01acdb53a0c3a3c957e38835f25cda5490a0
Szymon Skazinski <szyskazi@gmail.com>
2020-04-20 22:41:15 +0200

    drinks class skeleton added

---
+++ src/main/java/com/infoshareacademy/Drinks.java
@@ -0,0 +1,45 @@
+package com.infoshareacademy;
+
+public class Drinks {
+
+    private String idDrink;
+    private String strDrink;
+    private String strCategory;
+    private String strAlcoholic;    //isAlcoholic?
+    private String strInstructions;
+    private String strDrinkThumb;   // URL to .jpg
+    private String strIngredient1;
+    private String strIngredient2;
+    private String strIngredient3;
+    private String strIngredient4;
+    private String strIngredient5;
+    private String strIngredient6;
+    private String strIngredient7;
+    private String strIngredient8;
+    private String strIngredient9;
+    private String strIngredient10;
+    private String strIngredient11;
+    private String strIngredient12;
+    private String strIngredient13;
+    private String strIngredient14;
+    private String strIngredient15;
+    private String strMeasure1;
+    private String strMeasure2;
+    private String strMeasure3;
+    private String strMeasure4;
+    private String strMeasure5;
+    private String strMeasure6;
+    private String strMeasure7;
+    private String strMeasure8;
+    private String strMeasure9;
+    private String strMeasure10;
+    private String strMeasure11;
+    private String strMeasure12;
+    private String strMeasure13;
+    private String strMeasure14;
+    private String strMeasure15;
+    private String dateModified;
+
+
+
+}
