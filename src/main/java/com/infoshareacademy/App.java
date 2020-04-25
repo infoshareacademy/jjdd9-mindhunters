5ed5273f54755e6e7de17b03673e40f6bde972ef
Szymon Skazinski <szyskazi@gmail.com>
2020-04-25 21:18:27 +0200

    Creating database from .json files

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -4,12 +4,17 @@ import com.infoshareacademy.domain.Drink;
 import com.infoshareacademy.domain.DrinksDatabase;
 import com.infoshareacademy.menu.MenuControl;
 
+import java.io.IOException;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         DrinksDatabase drinksDatabase = DrinksDatabase.getINSTANCE();
-        MenuControl menuControl = new MenuControl();
-        menuControl.mainNavigation();
+        //MenuControl menuControl = new MenuControl();
+       // menuControl.mainNavigation();
+
+        System.out.println(drinksDatabase.getDrinks().size());
+
     }
 }
 
