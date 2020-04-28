2f01b858c6c07f8d03c51613bb6e5fb735a5205e
kamil-markowski <kamilszmarkowski@gmail.com>
2020-04-28 19:29:14 +0200

    Add logo at the begin.

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,12 +1,16 @@
 package com.infoshareacademy;
 
+import com.infoshareacademy.menu.DisplayMenu;
 import com.infoshareacademy.menu.MenuControl;
+import com.infoshareacademy.utilities.Utilities;
 
 import java.io.IOException;
 
 public class App {
 
     public static void main(String[] args) throws IOException {
+        DisplayMenu.logo();
+        Utilities.freezeConsole();
         MenuControl menuControl = new MenuControl();
         menuControl.mainNavigation();
     }
