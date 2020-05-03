a68943bdfcdb1ea613f08f68574cc00a2300922d
Lukasz Spyra <lukasz.spyrka@gmail.com>
2020-05-03 16:17:24 +0200

    Tests removed

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,29 +1,12 @@
 package com.infoshareacademy;
 
-import com.infoshareacademy.domain.DrinksDatabase;
 import com.infoshareacademy.menu.MenuControl;
-import com.infoshareacademy.service.DrinkCreator;
-import com.infoshareacademy.service.DrinkEditor;
-import com.infoshareacademy.service.DrinkRemover;
-import com.infoshareacademy.service.DrinkService;
 
 public class App {
 
     public static void main(String[] args) {
-        DrinkService.loadDrinkList();
         MenuControl menuControl = new MenuControl();
         menuControl.mainNavigation();
-
-        //tests
-        //DrinkCreator creator = new DrinkCreator();
-        //DrinkEditor editor = new DrinkEditor();
-        //DrinkRemover remover = new DrinkRemover();
-        //creator.createUserDrink();
-        //editor.editDrinkFromDatabase("17222");
-        //System.out.println(DrinksDatabase.getINSTANCE().getDrinks().get(0));
-        //System.out.println(remover.removeDrinkFromDatabase("17222"));
-        //System.out.println(DrinksDatabase.getINSTANCE().getDrinks().get(0));
-
 
     }
 }
