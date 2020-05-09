848b6cd25be389e305968b480138ba1ae0f48cc6
Szymon Skazinski <szyskazi@gmail.com>
2020-05-09 16:17:01 +0200

    junit-jupiter dependency fixed

--- src/test/java/com/infoshareacademy/service/SearchServiceTest.java
+++ src/test/java/com/infoshareacademy/service/SearchServiceTest.java
@@ -3,7 +3,7 @@ package com.infoshareacademy.service;
 import com.infoshareacademy.domain.Drink;
 import com.infoshareacademy.domain.Ingredient;
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
