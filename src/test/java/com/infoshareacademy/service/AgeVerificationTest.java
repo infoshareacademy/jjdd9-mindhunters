957ac95ad8ef5c95bbad02b4a87d58c8fc13bd79
michal-tubaja <m.tubaja@op.pl>
2020-05-10 21:08:05 +0200

    Walidacja Y/N 18yo

---
+++ src/test/java/com/infoshareacademy/service/AgeVerificationTest.java
@@ -0,0 +1,26 @@
+package com.infoshareacademy.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AgeVerificationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void saveAdultSessionTimestamp() {
+    }
+
+    @Test
+    void isAdultSessionActive() {
+    }
+}
