d2372aa920bc1d0fc6d17df1570a2ba3ae28b998
Lukasz Spyra <lukasz.spyrka@gmail.com>
2020-05-18 13:19:31 +0200

    Create servlet to test logger config

---
+++ web/src/main/java/com/infoshareacademy/servlets/LoggerServlet.java
@@ -0,0 +1,28 @@
+package com.infoshareacademy.servlets;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/loggers-test")
+public class LoggerServlet extends HttpServlet {
+
+    private static final Logger packageLogger = LoggerFactory.getLogger(LoggerServlet.class.getName());
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+
+        packageLogger.debug("Mindhunters Debug\n");
+        packageLogger.info("Mindhunters Info\n");
+        packageLogger.warn("Mindhunters Warn\n");
+        packageLogger.error("Mindhunters Error\n");
+
+    }
+}
