b758980de19358144cfd7bcb587d233e11a1ae77
Lukasz Spyra <lukasz.spyrka@gmail.com>
2020-05-29 17:55:16 +0200

    Create ingredients view-entity mapper

--- web/src/main/java/com/infoshareacademy/service/mapper/DrinkIgredientMapper.java
+++ web/src/main/java/com/infoshareacademy/service/mapper/DrinkIgredientMapper.java
@@ -1,20 +1,29 @@
 package com.infoshareacademy.service.mapper;
 
-import com.infoshareacademy.domain.Category;
 import com.infoshareacademy.domain.DrinkIngredient;
-import com.infoshareacademy.domain.dto.CategoryView;
 import com.infoshareacademy.domain.dto.DrinkIngredientView;
 
 import javax.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestScoped
 public class DrinkIgredientMapper {
 
     public DrinkIngredientView toView(DrinkIngredient drinkIngredient) {
-      DrinkIngredientView view = new DrinkIngredientView(
-              drinkIngredient.getIngredient().getName(),
-              drinkIngredient.getMeasure().getQuantity()
-      );
-      return view;
+        DrinkIngredientView view = new DrinkIngredientView(
+                drinkIngredient.getIngredient().getName(),
+                drinkIngredient.getMeasure().getQuantity()
+        );
+        return view;
     }
+
+    public List<DrinkIngredientView> toView(List<DrinkIngredient> drinkIngredients) {
+        List<DrinkIngredientView> drinkIngredientViews = new ArrayList<>();
+        for (DrinkIngredient drinkIngredient : drinkIngredients) {
+            drinkIngredientViews.add(toView(drinkIngredient));
+        }
+        return drinkIngredientViews;
+    }
+
 }
