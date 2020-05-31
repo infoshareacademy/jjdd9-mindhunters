8e605c39837589fe5f2246e194f212a4d41013f3
Szymon Skazinski <szyskazi@gmail.com>
2020-06-01 00:23:01 +0200

    Refactor and some visual changes

--- web/src/main/webapp/js/receipeList.js
+++ web/src/main/webapp/js/receipeList.js
@@ -1,79 +1,47 @@
 $("#myID2").click(function myFunction() {
 
-/*
-    var searchParams = new URLSearchParams(window.location);
-*/
-/*
-
-    for (let p of searchParams) {
-        console.log(p);
-    }*/
-
-    let url = new URL(window.location); // or construct from window.location
+    let url = new URL(window.location);
 
     let content = $(event.target).text().trim();
 
-  //  console.log(content);
     var searchParams = url.searchParams;
 
     searchParams.set("page", "1")
 
-    if (!searchParams.getAll("category").includes(content)){
-
+    if (!searchParams.getAll("category").includes(content)) {
 
         searchParams.append("category", content);
-        window.location =  url;
-
-        $(event.target).classList.add('active');
-
+        window.location = url;
 
     } else {
 
         let searchArray = searchParams.getAll("category");
 
+        searchArray.splice(searchArray.indexOf(content), 1);
+        if (searchArray.length >= 1) {
+            var newUrl = "&category=" + searchArray.join("&category=");
 
-        searchArray.splice(searchArray.indexOf(content),1);
+        } else {
+            newUrl = "";
+        }
 
-        var newUrl = "&category=" + searchArray.join("&category=");
-
-
-        console.log(newUrl);
-
-        window.location =   '/list?' + newUrl;
+        window.location = '/list?page=1' + newUrl;
     }
 
 
-});
-
-
-
+})
 
 async function nextPage() {
     let url = new URL(window.location); // or construct from window.location
-    //   this.addClass("disabled");
 
     let params = new URLSearchParams(url.search.slice(1));
 
     let pageNumber = parseInt(params.get('page'));
 
-
     let newPageNumber = pageNumber + 1;
-
-    if (newPageNumber <= maxNumber) {
-        params.set('page', (newPageNumber).toString());
-        let newURL = 'http://localhost:8080/list?' + params.toString();
-
-
-        window.location(newURL);
-
-        if (pageNumber == 5) {
-
-            var element = document.getElementById("next");
-            element.classList.add("disabled");
-        }
-
-    }
-
+    params.set('page', (newPageNumber).toString());
+    let newURL = 'http://localhost:8080/list?' + params.toString();
+    location.replace(newURL);
 
 }
 
@@ -93,7 +61,6 @@ async function previousPage() {
     }
 }
 
-
 async function setPage() {
     let url = new URL(window.location); // or construct from window.location
 
@@ -107,26 +74,6 @@ async function setPage() {
         let newURL = 'http://localhost:8080/list?' + params.toString();
         location.replace(newURL);
 
-    }
-}
-
-
-function toDisabled() {
-
-    let url = new URL(window.location);
-    var element = document.getElementById("next");
-    element.classList.add("disabled");
-
-    let params = new URLSearchParams(url.search.slice(1));
-
-    let pageNumber = parseInt(params.get('page'));
-
-    console.info(pageNumber);
-
-    if (pageNumber == 5) {
-
-        var element = document.getElementById("next");
-        element.classList.add("disabled");
     }
 }
 
