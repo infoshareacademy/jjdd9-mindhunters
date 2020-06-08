f4fd3a7ae9cee36a9bc4b3e88279db9452c5e92a
Lukasz <lukasz.spyrka@gmail.com>
2020-06-08 18:47:33 +0200

    Add working jquery autocomplete plugin

--- web/src/main/webapp/js/ajaxCallForDrinks.js
+++ web/src/main/webapp/js/ajaxCallForDrinks.js
@@ -1,41 +1,17 @@
-document.getElementById("phrase").addEventListener('keyup', ajaxCallForDrinks);
-
-function ajaxCallForDrinks() {
-    console.log('inside My function');
-
-    var value = $(document.getElementById("phrase")).val();
-
-
-    console.log(document.getElementById("phrase").value);
-    console.log(document.getElementById("phrase").toString().length);
-
-    if (value.length > 2) {
-        console.log('inside text lenght > 2');
-        arr2 = [];
-        eventJsonToArrayOfNames($(document).ready(function () {
-            str1 = "http://localhost:8080/api/search/drink/";
-            res = str1;
-
-
-            str2 = str1.concat(document.getElementById("phrase").value);
-            res = str2;
-            console.log(res);
-            console.log('before ajax');
-            $.ajax({
-                url: res,
-                type: "GET",
-                success: function (result) {
-                    console.log('Success in converting data');
-                    results = result;
-                    console.log('after ajax');
-                    console.log(results);
-                    autocomplete(document.getElementById("phrase"), results, arr2);
-                },
-                error: function (error) {
-                    console.log(error);
-                }
+$('#input-name').keyup(function () {
+    console.log('inside js');
+    if (this.value.length < 3) return;
+    var substring = $(this).val();
+    $.ajax({
+        url: '/api/search/drink/' + substring,
+        type: 'GET',
+        success: function (data) {
+            console.log(data);
+            listOfNames = data;
+            let result = data.map(r => r.drinkName);
+            $('#input-name').autocomplete({
+                source: result
             });
-        }));
-
-    }
-}
+        }
+    });
+});    console.log('after js');
