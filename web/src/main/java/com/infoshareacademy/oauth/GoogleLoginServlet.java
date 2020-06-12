39b276a94f435dfb0e1a050df7e15f247327570c
Lukasz <lukasz.spyrka@gmail.com>
2020-06-12 17:08:37 +0200

    Create google login servlet

--- web/src/main/java/com/infoshareacademy/oauth/GoogleLoginServlet.java
+++ web/src/main/java/com/infoshareacademy/oauth/GoogleLoginServlet.java
@@ -2,26 +2,39 @@ package com.infoshareacademy.oauth;
 
 import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
 import com.google.api.client.extensions.servlet.auth.oauth2.AbstractAuthorizationCodeServlet;
+import com.google.api.client.http.GenericUrl;
 
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.UUID;
 
 @WebServlet("/login")
 public class GoogleLoginServlet extends AbstractAuthorizationCodeServlet {
+
+    @EJB
+    OAuthManager oAuthManager;
+
     @Override
     protected AuthorizationCodeFlow initializeFlow() throws ServletException, IOException {
-        return null;
+        return oAuthManager.buildFlow();
     }
 
     @Override
     protected String getRedirectUri(HttpServletRequest httpServletRequest) throws ServletException, IOException {
-        return null;
+
+        GenericUrl genericUrl = new GenericUrl(httpServletRequest.getRequestURL().toString());
+        genericUrl.setRawPath("/oauth2callback");
+
+        return genericUrl.build();
     }
 
     @Override
     protected String getUserId(HttpServletRequest httpServletRequest) throws ServletException, IOException {
-        return null;
+        return UUID.randomUUID().toString();
     }
+
+
 }
