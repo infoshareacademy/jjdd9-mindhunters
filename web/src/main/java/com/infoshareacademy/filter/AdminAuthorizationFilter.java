282717f1137c456c8f135a7618359e26f6ea8647
Lukasz <lukasz.spyrka@gmail.com>
2020-06-14 22:30:03 +0200

    Add AdminAuthorizationFilter

---
+++ web/src/main/java/com/infoshareacademy/filter/AdminAuthorizationFilter.java
@@ -0,0 +1,54 @@
+package com.infoshareacademy.filter;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(
+        filterName = "AdminAuthorizationFilter",
+        urlPatterns = {"/admin-users", "/adminPage", "/upload-json-api", "/upload-json-file", "/loggers-test", }
+)
+public class AdminAuthorizationFilter implements Filter {
+
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdminAuthorizationFilter.class.getName());
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+
+        final HttpSession httpSession = httpServletRequest.getSession(false);
+
+        if (httpSession==null){
+            LOGGER.info("Session expired.");
+            httpServletResponse.sendRedirect("/");
+            return;
+        }
+
+        final String role = String.valueOf(httpSession.getAttribute("role"));
+
+        boolean isAuthorized = (role.equals("ADMIN") || role.equals("SUPER_ADMIN"));
+
+        if (!isAuthorized) {
+            LOGGER.info("Unauthorized attempt to access admin restricted context");
+            httpServletResponse.sendRedirect("/error");
+            return;
+
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+
+
+}
