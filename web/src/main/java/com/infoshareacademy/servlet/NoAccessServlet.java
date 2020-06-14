83ea52d528ed5c29d8a53b9205caa58e2dd3917a
Lukasz <lukasz.spyrka@gmail.com>
2020-06-14 22:31:23 +0200

    Add NoAccessServlet and Error page

---
+++ web/src/main/java/com/infoshareacademy/servlet/NoAccessServlet.java
@@ -0,0 +1,46 @@
+package com.infoshareacademy.servlet;
+
+import com.infoshareacademy.context.ContextHolder;
+import com.infoshareacademy.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/error")
+public class NoAccessServlet extends HttpServlet {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggerServlet.class.getName());
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+        Map<String, Object> dataModel = new HashMap<>();
+
+        ContextHolder contextHolder = new ContextHolder(req.getSession());
+        dataModel.put("name", contextHolder.getName());
+        dataModel.put("role", contextHolder.getRole());
+
+        Template template = templateProvider.getTemplate(getServletContext(), "error.ftlh");
+
+        try {
+            template.process(dataModel, resp.getWriter());
+        } catch (TemplateException e) {
+            LOGGER.warn("Template not created");
+        }
+
+    }
+}
