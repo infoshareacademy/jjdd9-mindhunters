85f7218b4240e8d2c87737d6029b931b1d534b3d
Szymon Skazinski <szyskazi@gmail.com>
2020-06-15 23:02:43 +0200

    Added UTF-8 charset to DrinkListServlet

--- web/src/main/java/com/infoshareacademy/servlet/DrinkListServlet.java
+++ web/src/main/java/com/infoshareacademy/servlet/DrinkListServlet.java
@@ -50,6 +50,7 @@ public class DrinkListServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html; charset=UTF-8");
 
         String pageNumberReq = req.getParameter("page");
