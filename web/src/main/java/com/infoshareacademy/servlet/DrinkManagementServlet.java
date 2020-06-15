09db3e8b2c5746ee89d3d5aefa4be7253bfda44e
Szymon Skazinski <szyskazi@gmail.com>
2020-06-16 01:21:47 +0200

    Updated add drink form and added button to drink management to navbar

--- web/src/main/java/com/infoshareacademy/servlet/DrinkManagementServlet.java
+++ web/src/main/java/com/infoshareacademy/servlet/DrinkManagementServlet.java
@@ -1,12 +1,15 @@
 package com.infoshareacademy.servlet;
 
 import com.infoshareacademy.context.ContextHolder;
-import com.infoshareacademy.domain.Category;
-import com.infoshareacademy.domain.Drink;
+import com.infoshareacademy.domain.*;
 import com.infoshareacademy.domain.dto.FullDrinkView;
+import com.infoshareacademy.domain.dto.IngredientView;
 import com.infoshareacademy.freemarker.TemplateProvider;
+import com.infoshareacademy.mapper.IngredientMapper;
 import com.infoshareacademy.service.CategoryService;
 import com.infoshareacademy.service.DrinkService;
+import com.infoshareacademy.service.IngredientService;
+import com.infoshareacademy.service.MeasureService;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import org.slf4j.Logger;
@@ -19,12 +22,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-@WebServlet("/drink_management")
+@WebServlet("/drink-management")
 public class DrinkManagementServlet extends HttpServlet {
 
     private static final Logger packageLogger = LoggerFactory.getLogger(DrinkManagementServlet.class.getName());
@@ -38,6 +45,12 @@ public class DrinkManagementServlet extends HttpServlet {
     @EJB
     private CategoryService categoryService;
 
+    @EJB
+    private IngredientService ingredientService;
+
+    @EJB
+    private MeasureService measureService;
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -45,11 +58,13 @@ public class DrinkManagementServlet extends HttpServlet {
 
         Map<String, Object> dataModel = new HashMap<>();
 
+        dataModel.put("categories", categoryService.findAllCategories());
+
         ContextHolder contextHolder = new ContextHolder(req.getSession());
         dataModel.put("name", contextHolder.getName());
         dataModel.put("role", contextHolder.getRole());
 
-        Template template = templateProvider.getTemplate(getServletContext(), "addDrinkForm.html");
+        Template template = templateProvider.getTemplate(getServletContext(), "addDrinkForm.ftlh");
 
         try {
             template.process(dataModel, resp.getWriter());
@@ -68,6 +83,25 @@ public class DrinkManagementServlet extends HttpServlet {
 
         drink.setDrinkName(req.getParameter("name"));
         drink.setRecipe(req.getParameter("recipe"));
+
+        String[] ingredients = req.getParameterValues("ingredient");
+
+        Ingredient ingredient = ingredientService.getOrCreate
+                (ingredients[0]);
+
+
+        String[] measures = req.getParameterValues("measure");
+
+        Measure measure = measureService.getOrCreate
+                (measures[0]);
+
+
+
+
+
+        //    drink.setDrinkIngredient(ingredientViewList);
+
+//        drink.setDrinkIngredient(List.of(drinkIngredient1,drinkIngredient));
         drink.setCategory(category);
         drink.setAlcoholStatus(req.getParameter("status"));
         drink.setDate(LocalDateTime.now());
