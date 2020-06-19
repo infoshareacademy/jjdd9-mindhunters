3f1b89449538f9c22943f6f3d700047a6f8ee5a1
Lukasz <lukasz.spyrka@gmail.com>
2020-06-19 17:02:48 +0200

    Create 3 pie charts for statististics

--- web/src/main/webapp/js/stats.js
+++ web/src/main/webapp/js/stats.js
@@ -1,154 +1,117 @@
-$( document ).ready(function(){
-    $.getJSON('/api/stats/drinks/category', function (data1) {
-        console.log(data1);
+var xLabels1 = [];
+var xLabels2 = [];
+var xLabels3 = [];
+
+var xData1 = [];
+var xData2 = [];
+var xData3 = [];
 
 
+function drawCharts(a, b, c) {
 
+    var ctx = document.getElementById(c);
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: a,
+            datasets: [{
+                label: 'Drinks',
+                data: b,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
     });
+}
 
 
+function getData() {
+    $.getJSON('/api/stats/drinks/category', function (data1) {
 
+        for (let i = 0; i < data1.length; i++) {
+            let pairDrinkQuantity1 = data1[i];
+            const name1 = pairDrinkQuantity1.name;
+            const quantity1 = pairDrinkQuantity1.quantity;
+            xLabels1.push(name1);
+            xData1.push(quantity1);
+
+        }
+        drawCharts(xLabels1, xData1, 'myChart1');
+    });
 
     $.getJSON('/api/stats/categories', function (data2) {
-        console.log(data2)
+
+        for (let i = 0; i < data2.length; i++) {
+            let pairDrinkQuantity2 = data2[i];
+            const name2 = pairDrinkQuantity2.name;
+            const quantity2 = pairDrinkQuantity2.quantity;
+            xLabels2.push(name2);
+            xData2.push(quantity2);
+
+        }
+        drawCharts(xLabels2, xData2, 'myChart2');
     });
-
-
 
 
     $.getJSON('/api/stats/drinks/top-10', function (data3) {
-        console.log(data3)
 
-        let drinkAndQuantityArray = [];
-        for (let i = 0; i < 1; i++) {
-            let pairDrinkQuantity = data3[0];
-            console.log(pairDrinkQuantity[0]);
-            drinkAndQuantityArray = [pairDrinkQuantity.get(0),
-                pairDrinkQuantity.get(1)];
-            console.log(drinkAndQuantityArray);
-
+        for (let i = 0; i < data3.length; i++) {
+            let pairDrinkQuantity3 = data3[i];
+            const name3 = pairDrinkQuantity3.name;
+            const quantity3 = pairDrinkQuantity3.quantity;
+            xLabels3.push(name3);
+            xData3.push(quantity3);
 
         }
-
-        xLabels.push(drinkAndQuantityArray);
-        var ctx = document.getElementById("myChart").getContext("2d");
-
+        drawCharts(xLabels3, xData3, 'myChart3');
     });
+}
 
 
+/*
+    $(document).ready(drawCharts());
+    console.log('koniec wywolanie drawCharts z getData');
+}
+*/
 
-});
 
-
-var ctx = document.getElementById('myChart');
-const xLabels = [];
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: xLabels,
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
+$(function () {
+    $(document).ready(function () {
+        getData();
+    });
 });
 
 
 /*
+$(document).ready(getData());
+$(document).ready(drawCharts());*/
 
-
-
-
-    ajax_chart(myChart, json_url);
-
-    // function to update our chart
-    function ajax_chart(chart, url, data) {
-        var data = data || {};
-
-        $.getJSON('/drinks/category', function (dataset1) {
-            console.log(dataset1)
-        }
-
-
-
-
-
-
-
-/!*
-            chart.data.labels = response.labels;
-            chart.data.datasets[0].data = response.data.quantity; // or you can iterate for multiple datasets
-            chart.update(); // finally update our chart*!/
-        });
-    }
-});
-
-
-*/
-
-
-/*
-
-
-
-
-
-    $.getJSON('/drinks/category', function (dataset1) {
-        let categoriesAndQuantitiesArrayChart1 = [['Category Name', 'Quantity',],];
-
-
-
-
-
-
-
-
-
-        /!*    for (let i = 0; i < dataset1.length; i++) {
-                let pairCategoryQuantity = dataset1[i];
-                let categoryAndQuantityArray = [pairCategoryQuantity.categoryName,
-                    pairCategoryQuantity.quantity];
-                categoriesAndQuantitiesArrayChart1.push(categoryAndQuantityArray);
-            }
-
-            let dataChart1 = google.visualization.arrayToDataTable(
-                categoriesAndQuantitiesArrayChart1);
-
-            var optionsChart1 = {
-                'title': 'Categories Ranking',
-                is3D: true
-            };*!/
-
-        let chart1 = new google.visualization.PieChart(
-            document.getElementById('piechart1'));
-
-        chart1.draw(dataChart1, optionsChart1);
+/*$(document).ready(function () {
+    $("#testowy").click(function (e) {
+        e.preventDefault();
+        drawCharts();
     });
-};*!/
-*/
+});*/
+
+
+
+
+
+
+
+
