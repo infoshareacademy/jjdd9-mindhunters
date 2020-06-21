fdc46ef398867ffc4152f9416047a27c137facd7
Szymon Skazinski <szyskazi@gmail.com>
2020-06-21 20:37:21 +0200

    Add possibility to adding more ingredients in editDrinkForm.ftlh.

--- web/src/main/webapp/js/editDrink.js
+++ web/src/main/webapp/js/editDrink.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
     $('#submit_but').click(function () {
+
+
         if (confirm('Do you really want to edit this recipe?')) {
 
             let url = new URL(window.location);
@@ -36,19 +38,33 @@ $(document).ready(function () {
     var maxField = 10; //Input fields increment limitation
     var addButton = $('.add_button'); //Add button selector
     var wrapper = $('.add-group'); //Input field wrapper
+    var initialIngredientsCount = document.getElementById("initialIngredientsCount").textContent.trim().toString();
+
+    var x = initialIngredientsCount; //Initial field counter is 1
+
     var fieldHTML =
-        '<div class="list-group-item sidebar-list" >' +
+        '<div class=" sidebar-list" >' +
         '<div class="row justify-content-between " id="ingrMeasure" style="margin: 0px">' +
-        '<input type="text" required maxlength="20" minlength="2" name="ingredient" value="" placeholder="Ingredient..." class="form-control md-3 col-5"/>'+
-        '<input type="text" required maxlength="20" minlength="2" name="measure" value="" placeholder="Measure..." class="form-control md-3 col-5"/>' +
+
+        '<div class=" md-3 col-5 ">' +
+        '<input type="text" required maxlength="20" minlength="2" name="drinkIngredients[' + x.toString().trim() +  '][ingredient[name]]" value="" placeholder="Ingredient..." class="form-control"/>'+
+        '<div class="invalid-feedback alert alert-danger">Input ingredient</div>\n' +
+        '</div>' +
+
+        '<div class=" md-3 col-5 ">' +
+        '<input type="text" required maxlength="20" minlength="2" name="drinkIngredients[' + x.toString().trim() +'][measure[quantity]]" value="" placeholder="Ingredient..." class="form-control"/>'+
+        '<div class="invalid-feedback alert alert-danger">Input measure</div>\n' +
+        '</div>' +
+
+
         '<a id="remove_blue" href="javascript:void(0);"' +
-        ' class="remove_button">' +
+        ' class="remove_button col-1">' +
         '<svg class="bi bi-trash" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">\n' +
         '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>\n' +
         '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>\n' +
         '</svg><br></a></div>'; //New input field html
 
-    var x = 1; //Initial field counter is 1
+
 
     //Once add button is clicked
     $(addButton).click(function () {
