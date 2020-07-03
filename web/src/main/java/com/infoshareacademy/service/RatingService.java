944235147914de6aefa3458891903f45feb269c3
Lukasz <lukasz.spyrka@gmail.com>
2020-07-03 12:07:24 +0200

    Fix rating method to set it to 0 when no rating

--- web/src/main/java/com/infoshareacademy/service/RatingService.java
+++ web/src/main/java/com/infoshareacademy/service/RatingService.java
@@ -1,11 +1,11 @@
 package com.infoshareacademy.service;
 
 import com.infoshareacademy.domain.Rating;
+import com.infoshareacademy.repository.DrinkRepository;
 import com.infoshareacademy.repository.RatingRepository;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import java.util.function.Supplier;
 
 @Stateless
 public class RatingService {
@@ -13,28 +13,36 @@ public class RatingService {
     @EJB
     private RatingRepository ratingRepository;
 
-
-    public Rating getRatingByDrinkId(Long drinkId){
-
-        return ratingRepository.findByDrinkId(drinkId).orElse(createEmptyRating());
-
-    }
+    @EJB
+    private DrinkRepository drinkRepository;
 
 
-    public Double getCalculatedRatingByDrinkId(Long drinkId){
+    public Rating getRatingByDrinkId(Long drinkId) {
 
-        final Rating rating = ratingRepository.findByDrinkId(drinkId).orElse(createEmptyRating());
-
-        return (double)rating.getSum()/rating.getNumberOfRatings();
+        return ratingRepository.findByDrinkId(drinkId).orElse(createEmptyRating(drinkId));
 
     }
 
 
-    private Rating createEmptyRating() {
+    public Double getCalculatedRatingByDrinkId(Long drinkId) {
+
+        final Rating rating = ratingRepository.findByDrinkId(drinkId).orElse(createEmptyRating(drinkId));
+
+        if (rating.getSum() == 0){
+            return 0.0;
+        }
+
+        return (double) rating.getSum() / rating.getNumberOfRatings();
+
+    }
+
+
+    private Rating createEmptyRating(Long drinkId) {
 
         Rating initialRating = new Rating();
         initialRating.setNumberOfRatings(0L);
         initialRating.setSum(0L);
+        initialRating.setDrink(drinkRepository.findDrinkById(drinkId));
 
         return initialRating;
     }
